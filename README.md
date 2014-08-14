bb142254d501653237ab1df0d26842b35e14befb
Linsen <jasper.linsen@turner.com>
2014-08-14 12:08:45 +0100

    ADDED README AND INITIAL PLUGIN

--- README.md
+++ README.md
@@ -1,4 +1,92 @@
 strappon-audio
 ==============
 
-Easely use the web audio API
+#Strappon Audio
+##Introduction
+
+The Strappon Audio plugin allows easy, cross-platform web audio performance. For maximum compatibility it is best to initiate the audio API after the user has clicked a button, assuring that audio will be available on mobile devices as well.
+
+##Usage
+
+###Including the API in your page
+
+Including the API into your page is as easy as loading it in a script tag. You can do this anywhere but it is recommended to do so on in the *<head>*tag.
+
+`
+<script type="text/javascript" src="folder/structure/strappon.audio.js"></script>
+`
+As long as you initialise your audio after including this file, it will work. Initialising your audio is as easy as calling a new `sound ()` object and passing it some parameters. By default debugging is on, but if you turn debug on you will see instructions on how to work with the API. You can always call the `help()`function to get more info on any of the API functions, or use this document as reference.
+
+`
+<script type="text/javascript">
+	var _audio = new sound({
+url:"folder/structure/file.wav",
+debug:true,
+instances : {
+	"instanceName" : { "start" : 0, "duration" : 2 },
+}
+	});
+</script>
+`
+By turning debug on initialisation, debugging will report a lot of things that are going on. This will only work on browsers with a *console</span>, and it will throw an alert on browser that don't support *console.log</span>.
+
+There are three major ways in which you can play audio: play, loop and consecutive. 
+`Play` will simply play the audio and stop after the first play. You can pull it as often as you want and is useful for pulling at certain events. 
+`Loop` will loop the audio until either another sound is played using play, loop or consecutive, or until `stop()` is called. 
+`Consecutive` allows stacking of audio one after the other. This allows you to create a chain that ends in a loop by passing an array that either contains strings (every string will be played in order), or arrays (where the first element is the instance and the second is either play or loop). Once a loop element is found, it will stop executing the consecutive function.
+
+`
+<script type="text/javascript">
+	_audio.play("instanceName");
+	_audio.loop("instanceName");
+	_audio.consecutive([
+"instanceName",
+["instanceName","play"],
+["instanceName","loop"]
+	]);
+</script>
+`
+
+Any audio events fired before the file is ready will be stacked and played consecutively as soon as the file is loaded in. You can check whether the file is ready by polling the `ready` variable. You can also redefine the `onready` as a function to be pulled when ready by passing a `{ready : function(){}` function on initialisation.
+
+##API
+
+###fn.INIT
+
+	`var.init($url,$instances)` 
+	Initialise audio.
+	`$url` {STR} Location of file.
+	`$instances` {OBJ: {$name : {$start, $duration}}}: Object containing named objects with a start and duration (floats).
+###fn.TRACK
+
+	`var.track($url)`
+	Initialise audio file.
+	`$url` {STR} Location of file.
+###fn.ADD
+
+	`var.add($name,$start,$duration)`
+	Add instance of audio.
+	`$name` {STR} Name of this instance.
+	`$start` {INT/FLOAT}: Point in track where instance starts (seconds).
+	`$duration` {INT/FLOAT}: Length of instance.
+###fn.PLAY
+
+	`var.play($instance)`
+	Play instance.
+	`$instance` {STR} Name of instance to be played.
+###fn.STOP
+
+	`var.stop()`
+	Stop audio.
+###fn.LOOP
+
+	`var.loop($instance)`
+	Loop instance.
+	`$instance` {STR} Name of instance to be looped.
+###fn.CONSECUTIVE
+
+	`var.consecutive($instances)`
+	Play instances consecutively.
+	`$instances` {ARRAY} Array consisting of either strings (to play one after the other), or other arrays ([$instance, $type]).
+	`$instance` {STR} : Name of instance.
+	`$type` {STR} : Play type (loop or play).
---
+++ strappon.audio.js
@@ -0,0 +1,267 @@
+/******************************************************
+	js.audio.js
+		started on 27/04/2014
+		latest update on 07/08/2014
+		by Jasper Linsen
+******************************************************/
+
+function sound(object){
+	var url = object.url ? object.url : false;
+	var instances = object.instances ? object.instances : false;
+	var debug = object.debug ? object.debug : false;
+	var ready = object.ready ? object.ready : false;
+	this.init(url,instances,debug,ready);
+}
+sound.prototype = {
+	context : false,
+	buffer : null,
+	instances : {},
+	source : null,
+	element : null,
+	timeout : null,
+	muteValue : false,
+	mute : function(a){
+		if(a === undefined) return this.muteValue;
+		else if(a === false || a === true) this.muteValue = a;
+		else this.debug("fn.MUTE variable passed is not a boolean");
+	},
+	playing : false,
+	ready : false,
+	onready : [],
+	debugValue : false,
+	debug : function(a){
+		if(a === undefined) return this.debugValue;
+		else if(a === false || a === true) this.debugValue = a;
+		else if(this.debug() && console) console.log(a);
+		else if(this.debug()) throw(a);
+	},
+	help : function(fn){
+		if(console){ 
+			var functions = {
+				init : "INIT\nvar.init($url,$instances). Initialise audio.\n   $url {STR} Location of file.\n   $instances {OBJ: {$name : {$start, $duration}}}: Object containing named objects with a start and duration (floats).\n  ",
+				track : "TRACK\nvar.track($url). Initialise audio file.\n   $url {STR} Location of file.\n  ",
+				add : "ADD\nvar.add($name,$start,$duration). Add instance of audio.\n   $name {STR} Name of this instance.\n   $start {FLOAT}: Point in track where instance starts (seconds).\n   $duration {FLOAT}: Length of instance.\n  ",
+				play : "PLAY\nvar.play($instance). Play instance.\n   $instance {STR} Name of instance to be played.\n  ",
+				stop : "STOP\nvar.stop(). Stop audio.\n  ",
+				loop : "LOOP\nvar.loop($instance). Loop instance.\n   $instance {STR} Name of instance to be looped.\n  ",
+				consecutive : "CONSECUTIVE\nvar.consecutive($instances). Play instances consecutively.\n   $instances {ARRAY} Array consisting of either strings (to play one after the other), or other arrays ([$instance, $type]).\n       $instance {STR} : Name of instance.\n       $type {STR} : Play type (loop or play).\n  "
+			}
+			console.log("------------    HELP   ------------- \n  ");
+			if(fn && functions[fn]) console.log(functions[fn]);
+			else {
+				for(i in functions) console.log(functions[i]);
+			}
+			console.log("------------- END HELP -------------");
+		}
+		else alert("fn.HELP: Help is available on browsers with a console.");
+	},
+	init : function(url,instances,debug,ready){
+		if(debug) this.debug(true);
+		if(url) this.track(url);
+		else this.debug("fn.INIT: No URL passed on init. Add one with 'var.track(url)'.");
+		if(instances){
+			for(var i in instances){
+				this.add(i, instances[i]["start"], instances[i]["duration"]);
+			}
+ 		}
+ 		else this.debug("fn.INIT: No instances passed on init. Add one with 'var.add(name,start,duration)'.");
+ 		if(typeof ready === "function") this.onready = ready;
+	},
+	track : function(url){
+		if(!url){
+			debug("fn.TRACK: No file url passed.");
+			return;
+		}
+		var local = this;
+		try {
+			var request = new XMLHttpRequest();
+  			request.open('GET', url, true);
+  			request.responseType = 'arraybuffer';
+  		
+			window.AudioContext = window.AudioContext || window.webkitAudioContext;
+			local.context = new AudioContext();
+		
+  			// Decode asynchronously
+ 			request.onload = function(){
+				local.context.decodeAudioData(request.response, function(buffer) {
+					local.buffer = buffer;
+					// connect to the buffer so the first noteOff doesnt error
+					local.source = local.context.createBufferSource();
+					local.source.buffer = local.buffer;
+					local.source.connect(local.context.destination);
+					local.ready = true;
+					local.debug("fn.TRACK: Audio file ready for use.");
+					if(typeof local.onready === "function") local.onready();
+					else local.consecutive(local.onready);
+				}, function(){});
+ 			};
+ 			request.send();
+ 			local.element = new Audio(url);
+ 			local.element.load();
+ 		} catch(e){
+ 			local.debug("fn.TRACK: Audio is not supported on your device. See message below.");
+ 			local.debug(e);
+ 		}
+	},
+	add : function(name,start,duration){
+		var local = this;
+		if(!name){
+			local.debug("fn.ADD: No instance name passed.");
+			return;
+		}
+		if(!start && start !== 0){
+			local.debug("fn.ADD: No instance start point passed.");
+			return;
+		}
+		if(!duration){
+			local.debug("fn.ADD: No instance duration passed.");
+			return;
+		}
+		if(local.instances[name] != undefined){
+			var i = local.instances[name];
+			local.debug("fn.ADD: Instance '" + name + "' already existed (start: " + i["start"] + ", duration:" + i["duration"] + "). Instance overwritten.");
+		}
+		local.instances[name] = {
+ 			"start" : start,
+ 			"duration" : duration
+ 		};
+	},
+	play : function(instance){
+		var local = this;
+		local.debug("fn.PLAY: Trying to play '" + instance + "'...");
+		if(!local.ready){ 
+			if(typeof local.onready === "function"){
+				local.debug("fn.PLAY: Audio not ready yet. Onready function will be triggered as soon as ready.");
+			} else {
+				local.debug("fn.PLAY: Audio not ready yet. Queued to play as soon as ready.");
+				local.onready.push([instance,"play"]);
+			}
+			return;
+		}
+		if(local.mute()){
+			local.debug("fn.PLAY: Audio is muted (will not play)");
+			return;
+		}
+		if(!instance){
+			local.debug("fn.PLAY: No instance passed to play option.");
+			return;
+		} else {
+			success = this.instances[instance] != undefined
+			? this.instances[instance]
+			: false;
+			if(!success){
+				local.debug("fn.PLAY instance was not found.")	;
+				return;
+			}
+		}
+		local.playing = true;
+		var start = success["start"], 
+			stop = success["duration"];
+		try {
+			local.source = local.context.createBufferSource();
+			local.source.buffer = local.buffer;
+			local.source.connect(local.context.destination);
+			local.source.start(0,start,stop);
+		} catch(e) {
+			local.debug("fn.PLAY: Error playing type 1 audio, trying type 2. Error below.");
+			local.debug(e);
+			try {
+				clearTimeout(this.timeout);
+				local.element.pause();
+				local.element.currentTime = start;
+				local.element.play();
+				this.timeout = setTimeout(function(){
+					local.element.pause();
+				}, 1000 * stop);
+			} catch(e){
+				// silently fail here
+				local.debug("fn.PLAY: Error playing type 2 audio. Giving up. Error below.");
+				local.debug(e);
+			}
+		}
+	},
+	stop : function(){
+		var local = this;
+		local.debug("fn.STOP Stopping all audio.");
+		try {
+			local.source.stop();
+		} catch(e){
+			try {
+				local.element.pause();
+			} catch(e){}
+		}
+		local.playing = false;
+		clearInterval(local.looping);
+		local.looping = false;
+		clearTimeout(local.consecutiveTimeout);
+		local.consecutiveTimeout = false;
+	},
+	looping : false,
+	loop : function(id){
+		var local = this;
+		local.debug("fn.LOOP Looping instance '" + id + "'.");
+		if(id && local.instances[id] != undefined){
+			local.stop();
+			local.play(id);
+			local.looping = setInterval(function(){
+				local.play(id);
+			}, local.instances[id].duration * 1000);
+		} else {
+			clearInterval(local.looping);
+			local.looping = false;
+		}
+	},
+	consecutiveTimeout : false,
+	consecutive : function(order, current){
+		var local = this;
+		clearTimeout(local.consecutiveTimeout);
+		local.consecutiveTimeout = false;
+		if(typeof order === "object"){
+			current = current || 0;
+			var id = false;
+			var action = "play";
+			var pause = 0;
+		// if the value is an array of [id,loop?,pause]
+			if(typeof order[current] === "object"){
+		// if the first value in the array is there and its a known instance, set it
+				if(order[current].length >= 1 && local.instances[order[current][0]] != undefined){
+					id = order[current][0];
+					pause = local.instances[order[current][0]].stop * 1000;
+				}
+		// if the second variable is a string and the action exists and is a function, apply the action
+				if(
+					order[current].length >= 2 
+					&& typeof order[current][1] === "string" 
+					&& typeof local[order[current][1]] === "function"
+				){
+					action = order[current][1];
+				} else {
+					local.debug("fn.CONSECUTIVE Action does not exists " + order[current][1]);
+				}
+				if(
+					order[current].length >= 3 
+					&& parseInt(local.instances[order[current][2]]) != "NaN"
+				){
+					id = order[current][0];
+					pause += local.instances[order[current][2]];
+				}
+			} else if(typeof order[current] === "string"){
+				if(local.instances[order[current]] != undefined)
+					id = order[current][0];
+			} else {
+				local.debug("fn.CONSECUTIVE: Nothing defined in action");
+				return;
+			}
+			local[action](id);
+			local.debug("fn.CONSECUTIVE: executing '" + action + "' with id '" + id + "'");
+			current++;
+			if(order[current] != undefined && action !== "loop"){
+				local.consecutiveTimeout = setTimeout(function(){
+					local.consecutive(order,current);
+				}, pause);
+			}
+		} else {
+			local.debug("fn.CONSECUTIVE: Object passed to audio.consecutive is not an array of arrays. (type is " + typeof order + ")");
+		}
+	}
+};
